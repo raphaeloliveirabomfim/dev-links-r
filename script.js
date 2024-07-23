9fe9fa1d3fbd51098c06c0fa2747a81f0cd2fdfc
Raphael Oliveira Bomfim <rapha.sep@hotmail.com>
2024-07-23 10:19:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+   const html = document.documentElement
+    html.classList.toggle("light")
+  
+ const img = document.querySelector("#profile img") 
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', 'avatar.png')
+    } else {
+        img.setAttribute('src', 'avatar.png')
+    }
+ }
